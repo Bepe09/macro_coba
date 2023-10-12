5414bd6ef47c73d2539afc8ad57187dd4b223885
Nam Do-San <bputrantio@student.ciputra.ac.id>
2023-10-12 15:31:03 +0700

    Initial Commit

---
+++ Macro_coba1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Macro_coba1
+//
+//  Created by Brian Putrantio on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Macro_coba1/Macro_coba1App.swift
@@ -0,0 +1,17 @@
+//
+//  Macro_coba1App.swift
+//  Macro_coba1
+//
+//  Created by Brian Putrantio on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Macro_coba1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
